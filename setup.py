f28a18951badf97a1732e35235f160c37def72f4
Christian Kauhaus <kc@gocept.com>
2010-06-12 17:24:22 +0200

    Initial package skeleton

---
+++ setup.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2010 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='nagiosplugin',
+    version='0.1dev',
+    description='Framework for creating Nagios plugins',
+    author='Christian Kauhaus',
+    author_email='kc@gocept.com',
+    license='GPL-3',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False
+)
---
+++ src/nagiosplugin/plugin.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2010 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+__doc__ = """Framework for easy creation of Nagios plugins"""
+
+
+class Plugin(object):
+    pass
