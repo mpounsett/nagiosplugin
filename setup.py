ac0d4c11c40161fa38fbad4c91e71e9b1af7c6d3
Christian Kauhaus <kc@gocept.com>
2010-08-10 22:25:42 +0200

    Releasing v0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nagiosplugin',
-    version='0.2_beta_p1',
+    version='0.3',
     description='Class library for Python Nagios plugins',
     author='Christian Kauhaus',
     author_email='kc@gocept.com',
