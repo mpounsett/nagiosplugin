0f25d2de29c1bda9ab2d366e1883be88beda8733
Christian Kauhaus <kc@gocept.com>
2012-11-02 15:54:07 +0100

    Increase test coverage for Metric and Performance

---
+++ src/nagiosplugin/tests/test_metric.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2012 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+from nagiosplugin.metric import Metric
+from nagiosplugin.context import Context
+import unittest
+
+
+class MetricTest(unittest.TestCase):
+
+    def test_description(self):
+        self.assertEqual('time is 1s', Metric(
+            'time', 1, 's', contextobj=Context('ctx')).description)
+
+    def test_valueunit_float(self):
+        self.assertEqual('1.302s', Metric('time', 1.30234876, 's').valueunit)
+
+    def test_valueunit_nonfloat(self):
+        self.assertEqual('text', Metric('text', 'text').valueunit)
+
+    def test_evaluate_fails_if_no_context(self):
+        with self.assertRaises(RuntimeError):
+            Metric('time', 1, 's').evaluate()
+
+    def test_performance_fails_if_no_context(self):
+        with self.assertRaises(RuntimeError):
+            Metric('time', 1, 's').performance()
---
+++ src/nagiosplugin/tests/test_performance.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2012 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+from nagiosplugin.performance import Performance
+import unittest
+
+
+class PerformanceTest(unittest.TestCase):
+
+    def test_normal_label(self):
+        self.assertEqual('d=10', str(Performance('d', 10)))
+
+    def test_label_too_long(self):
+        with self.assertRaises(RuntimeError):
+            str(Performance('d' * 21, 10))
+
+    def test_label_quoted(self):
+        self.assertEqual("'d d'=10", str(Performance('d d', 10)))
+
+    def test_label_must_not_contain_quotes(self):
+        with self.assertRaises(RuntimeError):
+            str(Performance("d'", 10))
